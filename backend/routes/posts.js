687fb24910fe6f7e3b30495280efbeb360eed31e
Gowri Sankar <sankaryadavrgukt@gmail.com>
2021-08-28 09:51:35 +0530

    completed posts api

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const app = require("express");
 const Post = require("../models/Post");
 const router = app.Router();
+const mongoose = require("mongoose");
 
 //@desc Create a post
 //POST  api/posts
@@ -38,6 +39,78 @@ router.put("/:id", async (req, res) => {
 		}
 	} catch (error) {
 		res.status(500).json(error);
+	}
+});
+
+//@desc delete the post by id
+//DELETE /api/posts/:id
+
+router.delete("/:id", async (req, res) => {
+	console.log("Post delete function is deleted");
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) return res.status(404).json("Not Found");
+		if (!post.username === req.body.username) {
+			return res.status(401).json("Unauthorized Deletion is NOT Possible");
+		}
+		try {
+			await post.delete();
+			return res.status(200).json("The Post has been deleted successfully");
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//@desc get post by id
+//GET /api/posts/:id
+
+router.get("/:id", async (req, res) => {
+	console.log("Get Post By Id is Triggered");
+	try {
+		if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+			return res.status(400).json("Enter Valid URL");
+		}
+		const post = await Post.findById(req.params.id);
+		if (!post) return res.status(404).json("Not Found");
+		return res.status(200).json(post);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+});
+
+//@desc get all post
+//GET /api/posts/
+
+router.get("/", async (req, res) => {
+	console.log("GET ALL Post is triggered");
+	const username = req.query.user;
+	const catName = req.query.cat;
+	try {
+		let posts;
+		if (username && catName) {
+			posts = await Post.find({
+				username,
+				categories: {
+					$in: [catName],
+				},
+			});
+		} else if (username) {
+			posts = await Post.find({ username });
+		} else if (catName) {
+			posts = await Post.find({
+				categories: {
+					$in: [catName],
+				},
+			});
+		} else {
+			posts = await Post.find();
+		}
+		res.status(200).json(posts);
+	} catch (err) {
+		return res.status(500).json(err);
 	}
 });
 
